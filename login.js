8ca241ac50c515473e23c6b38d0d7d635429833a
14harish <harishpc2003@gmail.com>
2022-08-22 21:48:03 +0530

    new

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-let express=require('express');
+let express=require("express");
 let exp=express();
 let sql=require('mysql');
 let bodyparser=require('body-parser');
@@ -8,19 +8,25 @@ let connect =sql.createConnection({
     user:'root',
     password:'root'
 })
-exp.use(bodyparser.urlencoded=({extended:true}));
+exp.use(bodyparser.urlencoded({extended:true}));
 
 exp.get("/",function(req,res){
     res.sendFile(__dirname+"/sigin.html");
 })
+exp.get("/sigin.html",function(req,res){
+    res.sendFile(__dirname+"/sigin.html");
+})
+exp.get("/signup.html",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
 exp.post("/insert",function(req,res){
     const input={
         username:req.body.username,
-        email:req.body.email,
+        Emailid:req.body.email,
         password:req.body.password
     }
     let sql=`insert login.signup set ?`;
-    connect.query(sql,input,function(err){
+    connect.query(sql,input,function(err,result){
         if(err) throw err;
         res.redirect("/");
     })
